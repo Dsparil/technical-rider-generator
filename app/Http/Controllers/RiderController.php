5281a01dd4268d12899e50ade7f0b5270c48f699
Dsparil <tox6mon@gmail.com>
2024-04-15 16:45:31 +0200

    Rider pdf

--- app/Http/Controllers/RiderController.php
+++ app/Http/Controllers/RiderController.php
@@ -68,9 +68,7 @@ class RiderController extends Controller
 
         $pdf = Pdf::loadView('rider.pdf', ['rider' => $rider]);
 
-        return $pdf->output();
-
-        //return $pdf->download($rider->band->name.'_rider_'.date('Ymd').'.pdf');
+        return $pdf->download($rider->band->name.'_rider_'.date('Ymd').'.pdf');
     }
 
     public function edit(?int $riderId, Request $request)
