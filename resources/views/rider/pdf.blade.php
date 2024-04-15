eaebdc4948f42ca7df4160b288561c1ac3a35fb8
Dsparil <tox6mon@gmail.com>
2024-04-15 16:25:48 +0200

    logo pdf

--- resources/views/rider/pdf.blade.php
+++ resources/views/rider/pdf.blade.php
@@ -46,7 +46,7 @@
 
         <main>
             <center>
-                <img src="{{ $rider->band->base64Logo() }}" height="50%" style="max-width: 90%" />
+                <img src="{{ $rider->band->base64Logo() }}" style="max-width: 90%; max-height: 50%;" />
                 <br />
                 <br />
                 <h1>Fiche technique</h1>
