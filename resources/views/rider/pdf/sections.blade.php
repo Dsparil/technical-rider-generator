299a86b2b6627a7ca1c3fac3c8ae585278f341b9
Dsparil <tox6mon@gmail.com>
2024-04-15 16:30:52 +0200

    Rider pdf

--- resources/views/rider/pdf/sections.blade.php
+++ resources/views/rider/pdf/sections.blade.php
@@ -1,12 +1,12 @@
-<table border="0" cellspacing="10" style="width: 100%">
+<table border="0" cellspacing="10">
     <tr>
-        <td style="vertical-align: top; text-align: justify; width: {{ (count($rider->sections) > 4)? '50%;' : '100%' }}">
+        <td style="vertical-align: top; text-align: justify; width: {{ (count($rider->sections) > 4)? '50%;' : '100%;' }}">
             @foreach($rider->sections as $riderSection)
             <h4 style="margin-bottom: 0">{{ $riderSection->title }}</h4>
             <div style="margin-top: 0">{!! $riderSection->content !!}</div>
                 @if($loop->iteration % 4 == 0)
         </td>
-        <td style="vertical-align: top; text-align: justify; width: {{ (count($rider->sections) > 4)? '50%;' : '100%' }}">
+        <td style="vertical-align: top; text-align: justify; width: {{ (count($rider->sections) > 4)? '50%;' : '100%;' }}">
                 @endif
             @endforeach
         </td>
